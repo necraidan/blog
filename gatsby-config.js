80db58164173626336675d95ec2786a9c0d07334
Benjamin Auzanneau <benjamin.auzanneau.tm@gmail.com>
2018-10-10 13:29:32 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-        title: 'Gatsby Default Starter'
+        title: 'Awesome Blog'
     },
     plugins: ['gatsby-plugin-react-helmet'],
     pathPrefix: `/blog`
