c6b7d4c766def4ea3d958aa98c5bac0786919bb6
BuildTools <benjamin.auzanneau.tm@gmail.com>
2019-11-07 00:07:43 +0100

    first release
    Add prod variable
    
    Fix linting and base href

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
     baseUrl:
       process.env.NODE_ENV === 'prod'
         ? 'https://necraidan.com/blog'
-        : 'http://localhost:3000'
+        : 'http://localhost:3000',
   },
   router: { base: '/blog' },
 
@@ -16,9 +16,9 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -35,7 +35,7 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -43,7 +43,7 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -52,10 +52,10 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'fr'
-    }
+      lang: 'fr',
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {},
 };
